85b58f9d7d8c9e43418df4b7b8b18f01e18bf195
Matt Calthrop <mcalthrop@gmail.com>
2017-06-11 12:04:55 +0100

    Refactor tests to use only one browser instance

--- src/test/java/com/mattcalthrop/sandbox/selenium/Selenium2ExampleIT.java
+++ src/test/java/com/mattcalthrop/sandbox/selenium/Selenium2ExampleIT.java
@@ -3,9 +3,7 @@ package com.mattcalthrop.sandbox.selenium;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,19 +12,41 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Selenium2ExampleIT {
-    private WebDriver driver;
-    private WebDriverWait wait;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private static String startUrl;
+
     private Boolean result;
 
-    @Before
-    public void setUp() {
+    private static void systemInfo() {
+        String browser = System.getProperty("browser");
+        String operatingSystem = System.getProperty("os.name");
+        String systemArchitecture = System.getProperty("os.arch");
+
+        System.out.println("");
+        System.out.println("Browser: " + browser);
+        System.out.println("OS: " + operatingSystem);
+        System.out.println("Architecture: " + systemArchitecture);
+        System.out.println("");
+        System.out.println("Tests now running...");
+        System.out.println("");
+    }
+
+    @BeforeClass
+    public static void beforeAll() throws Exception {
+        systemInfo();
+        startUrl = "http://www.google.co.uk/";
         driver = new ChromeDriver();
-        driver.get("http://www.google.co.uk/");
         wait = new WebDriverWait(driver, 10);
     }
 
-    @After
-    public void tearDown() {
+    @Before
+    public void beforeEach() throws Exception {
+        driver.get(startUrl);
+    }
+
+    @AfterClass
+    public static void afterAll() throws Exception {
         driver.close();
         driver.quit();
     }
